44f086f68560f9a42ff17dfc23d31d58588a014a
Spangenberg <jannes.spangenberg@uni-jena.de>
2022-10-07 12:02:11 +0200

    change order of sequences in stockholm output file

--- src/magnipore.py
+++ src/magnipore.py
@@ -384,7 +384,14 @@ def magnipore(mapping : dict, unaligned : dict, seqs_ids : tuple, alignment_sequ
 
     LOGGER.printLog(f'Writing marked alignment')
     fasta = itertools.chain(
-        [SeqIO.SeqRecord(Seq.Seq(''.join(seq)), id ='magnipore_marked_' + str((first_sample_label, sec_sample_label)[i]), name ='magnipore_' + str((first_sample_label, sec_sample_label)[i]), description='X=significant signal change, N=not significant') for i, seq in enumerate(alignment_sequences)], SeqIO.parse(open(alignment_path), 'fasta'))
+        [SeqIO.parse(open(alignment_path), 'fasta')] +
+        [SeqIO.SeqRecord(
+            Seq.Seq(''.join(seq)),
+            id ='magnipore_marked_' + str((first_sample_label, sec_sample_label)[i]),
+            name ='magnipore_' + str((first_sample_label, sec_sample_label)[i]),
+            description='X=significant signal change, N=not significant') for i, seq in enumerate(alignment_sequences)
+            ]
+        )
     stk = open(os.path.join(working_dir, first_sample_label + '_' + sec_sample_label + suffix + '_marked.stk'), 'w')
     SeqIO.write(fasta, stk, 'stockholm')
     

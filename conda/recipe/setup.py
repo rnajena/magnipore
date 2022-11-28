c777fefade6d0f43c99f948e93556f02c1ea369c
Jannes Spangenberg <janspang@gmx.de>
2022-11-28 17:05:43 +0100

    setup conda build

--- conda/recipe/setup.py
+++ conda/recipe/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Magnipore',
-      version='v1.0.0',
+      version='1.0.0',
       description='Compares two ONT sequences samples for differential signals cause by mutations and modifications',
       author='Jannes Spangenberg',
       author_email='jannes.spangenberg@uni-jena.de',

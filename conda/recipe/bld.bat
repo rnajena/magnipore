9dc0a1461ac344dd844ae444acf73c867fed3fbb
Jannes Spangenberg <janspang@gmx.de>
2022-11-28 15:50:44 +0100

    setup conda build

---
+++ conda/recipe/bld.bat
@@ -0,0 +1,2 @@
+"%PYTHON%" "%RECIPE_DIR%"/setup.py install
+if errorlevel 1 exit 1
--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,1 +1,1 @@
-$PYTHON setup.py install     # Python command to install the script.
+$PYTHON $RECIPE_DIR/setup.py install     # Python command to install the script.

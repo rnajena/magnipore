79560439aa80ddb04d0d4082bc99a1a2135a3a4d
Jannes Spangenberg <janspang@gmx.de>
2023-07-20 16:54:19 +0200

    Prepare 1.2.0

--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
 from setuptools import setup
 import versioneer
 
+# pypi dependencies
 requirements = [
-      'h5py>=3.7',
-      'biopython>=1.80',
-      'mafft>=7.508',
-      'matplotlib>=3.6',
-      'numpy>=1.23',
-      'scipy>=1.9',
-      'nanopolish>=0.14',
-      'minimap2>=2.24',
-      'samtools>=1.0',
-      'pandas>=1.5',
-      'seaborn>=0.12',
-      'psutil>=5.9',
-      'hdf5>=1.12',
-      'hdf5plugin>=3.3',
-      'ont_vbz_hdf_plugin>=1.0',
-      'pytest>=7.1',
-      ]
+    'h5py>=3.7',
+    'biopython>=1.80',
+    'mafft>=7.508',
+    'matplotlib>=3.6',
+    'numpy>=1.21',
+    'scipy>=1.9',
+    'minimap2>=2.24',
+    'samtools>=1.0',
+    'pandas>=1.5',
+    'seaborn>=0.12',
+    'psutil>=5.9',
+    'hdf5>=1.12',
+    'hdf5plugin>=3.3',
+    'vbz-h5py-plugin>=1.0',
+    'pytest>=7.1',
+    'gzip>=1.12',
+    'f5c>=1.2',
+    'read5>=1.2.0'
+]
 
 setup(
     name='magnipore',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="Compares two ONT sequences samples for differential signals cause by mutations and modifications.",
-    license="GNUv3",
+    license="GNU General Public License v3",
     author="Jannes Spangenberg",
     author_email='jannes.spangenberg@uni-jena.de',
     url='https://github.com/JannesSP/magnipore',
@@ -37,10 +40,16 @@ setup(
         ]
     },
     install_requires=requirements,
+    python_requires='>=3.8,<3.11',
     keywords=['Magnipore', 'magnipore', 'nanosherlock', 'ONT', 'Oxford Nanopore Technologies', 'MinION', 'Direct RNA Sequencing'],
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
     ]
 )

97746e46dccc971139d77ad8d15f49ca3862e813
Jannes Spangenberg <janspang@gmx.de>
2023-08-15 17:43:21 +0200

    Added description to cov_filter

--- magnipore/cov_filter.py
+++ magnipore/cov_filter.py
@@ -4,16 +4,20 @@
 # github: https://github.com/JannesSP
 # website: https://jannessp.github.io
 
+from magnipore.__init__ import __version__
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 import os
 import pandas as pd
 
 def parse() -> Namespace:
     parser = ArgumentParser(
-        formatter_class=ArgumentDefaultsHelpFormatter
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        description='A small script to filter the .magnipore output for a given coverage threshold. Please provide the path to the .magnipore file and a coverage threshold. All compared positions, where at least one sample has a coverage below your given threshold will be filtered out. The remaining positions are written to a file with the name of the provided magnipore file and the suffix \"_c10\": e.g. <given_magnipore_file>_c10.magnipore',
+        prog='Magnipore',
     )
     parser.add_argument('magnipore', type=str, help='.magnipore file to filter for a given coverage threshold')
     parser.add_argument('coverage', type=int, help='Coverage threshold to filter for. Results, where at least one sample has a coverage below the given threshold are filtered out. Results, where both samples have a coverage equal or higher than the threshold remain.')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s' + f' {__version__}')
     return parser.parse_args()
 
 def filter_coverage(magnipore_file : str, cov_threshold : int) -> None:

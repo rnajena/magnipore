3dc39139d026499deb95d2bbc67b96f0a3ee3343
Jannes Spangenberg <janspang@gmx.de>
2023-08-21 13:53:44 +0200

    Remove namespace

--- magnipore/__init__.py
+++ magnipore/__init__.py
@@ -1,11 +1,3 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-from magnipore import magnipore, nanosherlock, Helper
-
-__all__ = [
-    "magnipore",
-    "nanosherlock",
-    "Helper",
-]

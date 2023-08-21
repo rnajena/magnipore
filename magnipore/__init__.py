b054e101f736b7cf6e7aea590a1776acd6918b0e
Jannes Spangenberg <janspang@gmx.de>
2023-08-21 13:02:54 +0200

    Add modules to namespace

--- magnipore/__init__.py
+++ magnipore/__init__.py
@@ -1,3 +1,11 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+from magnipore import magnipore, nanosherlock, Helper
+
+__all__ = [
+    "magnipore",
+    "nanosherlock",
+    "Helper",
+]
